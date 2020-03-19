3bc80d2e2159abeed0a2ddc5df9f58bad9649608
SURASAK NONRURK <nongfew240241.1@gmail.com>
2020-03-19 22:53:30 +0700

    First

---
+++ src/config.js
@@ -0,0 +1,12 @@
+const config = {
+    apiKey: "AIzaSyD_pjsOICnDTayEWxM9a8lzXEWt3TFMjmk",
+    authDomain: "fir-072.firebaseapp.com",
+    databaseURL: "https://fir-072.firebaseio.com",
+    projectId: "fir-072",
+    storageBucket: "fir-072.appspot.com",
+    messagingSenderId: "572298346210",
+    appId: "1:572298346210:web:f8bb5acb1832f58b25e8bb",
+    measurementId: "G-L7NRZ6K69Z"
+  };
+
+  export default config
